01afa0e0a61a1da891ee6b4779c563bce7419310
cbedrosian1 <cbedrosian1@gmail.com>
2018-11-07 14:37:25 -0500

    Created IntConverter class

---
+++ WeatherDataAnalysis/Converter/IntConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace WeatherDataAnalysis.Converter
+{
+    /// <summary>
+    /// Converts integers to strings and back
+    /// </summary>
+    /// <seealso cref="Windows.UI.Xaml.Data.IValueConverter" />
+    public class IntConverter : IValueConverter
+    {
+        /// <summary>
+        /// Converts the specified value.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="targetType">Type of the target.</param>
+        /// <param name="parameter">The parameter.</param>
+        /// <param name="language">The language.</param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var temperature = (int) value;
+            return temperature.ToString();
+        }
+
+        /// <summary>
+        /// Converts the back.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="targetType">Type of the target.</param>
+        /// <param name="parameter">The parameter.</param>
+        /// <param name="language">The language.</param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            var tempText = (string) value;
+            return int.Parse(tempText);
+        }
+    }
+}
