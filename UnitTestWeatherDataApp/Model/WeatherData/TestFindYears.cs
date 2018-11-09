65e783220386f01561c050383075249d438f3e03
cbedrosian1 <cbedrosian1@gmail.com>
2018-11-08 23:57:21 -0500

    Fixed TestFindYears after method needed to be changed

--- UnitTestWeatherDataApp/Model/WeatherData/TestFindYears.cs
+++ UnitTestWeatherDataApp/Model/WeatherData/TestFindYears.cs
@@ -13,17 +13,17 @@ namespace UnitTestWeatherDataApp.Model.WeatherData
     {
 
         //  Input ({WeatherData.Days} in WeatherData)                                                   Expected output
-        //  [{1/1/2015,50,15,1}]                                                                        [2015]
-        //  [{1/1/2015,50,15,1}, {1/2/2016,45,25,1}]                                                    [2015,2016]
-        //  [{1/1/2015,50,15,1}, {1/2/2016,45,25,1}, {1/3/2017,40,20,1}, {1/4/2018,50,15,1}]            [2015,2016,2017,2018]
-        //  2x[{1/1/2015,50,15,1}, 2x{1/2/2016,45,25,1}, 2x{1/3/2017,40,20,1}, 2x{1/4/2018,50,15,1}]    [2015,2016,2017,2018]
+        //  [{1/1/2015,50,15,1}]                                                                        [{1/1/2015,50,15,1}]
+        //  [{1/1/2015,50,15,1}, {1/1/2016,45,25,1}]                                                    [1/1/2015,50,15,1},{1/1/2016,45,25,1}]
+        //  [{1/1/2015,50,15,1}, {1/1/2016,45,25,1}, {1/1/2017,40,20,1}, {1/1/2018,50,15,1}]            [{1/1/2015,50,15,1},{1/1/2016,45,25,1},{1/1/2017,40,20,1},{1/1/2018,50,15,1}]
+        //  2x[{1/1/2015,50,15,1}, 2x{1/1/2016,45,25,1}, 2x{1/1/2017,40,20,1}, 2x{1/1/2018,50,15,1}]    [{1/1/2015,50,15,1},{1/1/2016,45,25,1},{1/1/2017,40,20,1},{1/1/2018,50,15,1}]
         //  [{}]                                                                                        empty list
 
         #region Data members
 
         private WeatherDataAnalysis.Model.WeatherCalculator weatherData;
         private List<DailyStats> days;
-        private List<int> testList;
+        private List<DateTime> testList;
         private DailyStats day1;
         private DailyStats day2;
         private DailyStats day3;
@@ -37,18 +37,18 @@ namespace UnitTestWeatherDataApp.Model.WeatherData
         public void TestInit()
         {
             this.days = new List<DailyStats>();
-            this.testList = new List<int>();
+            this.testList = new List<DateTime>();
             this.day1 = new DailyStats(new DateTime(2015, 1, 1), 50, 15, 1);
-            this.day2 = new DailyStats(new DateTime(2016, 1, 2), 45, 25, 1);
-            this.day3 = new DailyStats(new DateTime(2017, 1, 3), 40, 30, 1);
-            this.day4 = new DailyStats(new DateTime(2018, 1, 4), 50, 15, 1);
+            this.day2 = new DailyStats(new DateTime(2016, 1, 1), 45, 25, 1);
+            this.day3 = new DailyStats(new DateTime(2017, 1, 1), 40, 30, 1);
+            this.day4 = new DailyStats(new DateTime(2018, 1, 1), 50, 15, 1);
         }
 
         [TestMethod]
         public void TestOneYearInData()
         {
             this.days.Add(this.day1);
-            this.testList.Add(this.day1.Date.Year);
+            this.testList.Add(this.day1.Date);
             this.weatherData = new WeatherDataAnalysis.Model.WeatherCalculator(this.days);
             var years = this.weatherData.FindYears();
             CollectionAssert.AreEquivalent(this.testList,
@@ -58,8 +58,8 @@ namespace UnitTestWeatherDataApp.Model.WeatherData
         [TestMethod]
         public void TestTwoYears()
         {
-            this.testList.Add(this.day1.Date.Year);
-            this.testList.Add(this.day2.Date.Year);
+            this.testList.Add(this.day1.Date);
+            this.testList.Add(this.day2.Date);
 
             this.days.Add(this.day1);
             this.days.Add(this.day2);
@@ -72,10 +72,10 @@ namespace UnitTestWeatherDataApp.Model.WeatherData
         [TestMethod]
         public void TestMultipleYears()
         {
-            this.testList.Add(this.day1.Date.Year);
-            this.testList.Add(this.day2.Date.Year);
-            this.testList.Add(this.day3.Date.Year);
-            this.testList.Add(this.day4.Date.Year);
+            this.testList.Add(this.day1.Date);
+            this.testList.Add(this.day2.Date);
+            this.testList.Add(this.day3.Date);
+            this.testList.Add(this.day4.Date);
 
             this.days.Add(this.day1);
             this.days.Add(this.day2);
@@ -90,10 +90,10 @@ namespace UnitTestWeatherDataApp.Model.WeatherData
         [TestMethod]
         public void TestMultipleSameYears()
         {
-            this.testList.Add(this.day1.Date.Year);
-            this.testList.Add(this.day2.Date.Year);
-            this.testList.Add(this.day3.Date.Year);
-            this.testList.Add(this.day4.Date.Year);
+            this.testList.Add(this.day1.Date.Date);
+            this.testList.Add(this.day2.Date.Date);
+            this.testList.Add(this.day3.Date.Date);
+            this.testList.Add(this.day4.Date.Date);
 
             this.days.Add(this.day4);
             this.days.Add(this.day1);
